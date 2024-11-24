060fb141db7b6d81e7f9b98353ab83530e52a818
sadrisso <sadriisso304@gmail.com>
2024-11-24 23:30:12 +0600

    Create client side post and Insert single user data

--- simple-CRUD-server/index.js
+++ simple-CRUD-server/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 6000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 7000;
 const app = express()
 
 
 app.use(cors())
 app.use(express.json())
 
+// vb7UmqvbjaQ.K2g
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://shoeb-drisso:<db_password>@cluster0.oq68b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const uri = "mongodb+srv://shoeb-drisso:vb7UmqvbjaQ.K2g@cluster0.oq68b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,12 +26,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("usersDB");
+    const usersCollection = database.collection("users");
+
+    app.post("/users", async(req, res) => {
+        const user = req.body;
+        console.log("New user: ", user)
+        const result = await usersCollection.insertOne(user);
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
