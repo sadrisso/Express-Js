12d8081638427ed14d087a8ae5e02c91a0da82d2
sadrisso <sadriisso304@gmail.com>
2024-11-27 22:56:54 +0600

    rendered milks using cards

--- milk-store/milk-store-client/src/components/Home.jsx
+++ milk-store/milk-store-client/src/components/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import MilkCard from './MilkCard';
 
 const Home = () => {
+
+    const milks = useLoaderData()
+    console.log(milks)
+
     return (
-        <div>
-            Home
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-5 my-10'>
+            {milks.map((milk, i) => <MilkCard milk={milk} key={i} />)}
         </div>
-    );
-};
+    )
+}
 
 export default Home;
--- milk-store/milk-store-client/src/main.jsx
+++ milk-store/milk-store-client/src/main.jsx
@@ -13,7 +13,8 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Home />
+        element: <Home />,
+        loader: () => fetch("http://localhost:3000/milks")
       },
       {
         path: "/addMilks",
