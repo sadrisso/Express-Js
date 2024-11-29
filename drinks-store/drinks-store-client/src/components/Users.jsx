4149129b70cb11692fe1a370e2a207d23a1b5470
sadrisso <sadriisso304@gmail.com>
2024-11-29 23:27:50 +0600

    user removed functionality from UI and DB

--- drinks-store/drinks-store-client/src/components/Users.jsx
+++ drinks-store/drinks-store-client/src/components/Users.jsx
@@ -7,6 +7,21 @@ const Users = () => {
     const loadedUser = useLoaderData()
     const [users, setUsers] = useState(loadedUser)
 
+    const handleRemove = (id) => {
+        console.log(id)
+
+        fetch(`http://localhost:9000/users/${id}`, {
+            method: "DELETE"
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.deletedCount > 0) {
+                    alert("Deleted")
+                }
+            })
+    }
+
     return (
         <div>
             <div className="overflow-x-auto">
@@ -29,7 +44,7 @@ const Users = () => {
                                     <td>{user.email}</td>
                                     <td className="flex gap-2">
                                         <button className="btn btn-success">Edit</button>
-                                        <button className="btn btn-error">Delete</button>
+                                        <button className="btn btn-error" onClick={() => handleRemove(user._id)}>Delete</button>
                                     </td>
                                 </tr>)
                         }
