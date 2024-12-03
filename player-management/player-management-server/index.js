9231511e5504f0cd44d7dc2ec2cd779d288be112
sadrisso <sadriisso304@gmail.com>
2024-12-03 08:01:20 +0600

    successfully connected to mongodb with environment variable

--- player-management/player-management-server/index.js
+++ player-management/player-management-server/index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 4000;
 const app = express()
 
 
+//middlewares
 app.use(cors())
 app.use(express.json())
+
+
+//MONGO DB CODE STARTS FROM HERE-------------------------------------------------------------------------------
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.oq68b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+//MONGO DB CODE ENDS HERE---------------------------------------------------------------------------------------
+
 
 
 
